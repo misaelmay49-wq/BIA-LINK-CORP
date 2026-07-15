8c93ad209a1c27b01dc525598eb200081f95cd56
misaelmay49-wq <misaelmay49@gmail.com>
2026-07-15 13:13:05 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify 
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
-from logica import registrar_producto, obtener_productos, procesar_venta_logica
+from logica import registrar_producto, obtener_productos, procesar_venta_logica, get_conn
 
 app = Flask(__name__)
 app.secret_key = 'bialink_clave_secreta_123'
