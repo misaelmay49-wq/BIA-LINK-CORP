e28c0e132179111db09606d5bcc6c250dfc7898c
misaelmay49-wq <misaelmay49@gmail.com>
2026-07-16 08:24:29 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -84,12 +84,12 @@ def registrar_venta():
 @app.route('/procesar_venta', methods=['POST'])
 def procesar_venta():
       if 'user_id' not in session:
-          return redirect(url_for('login'))
+        return redirect(url_for('login'))
 
-      usuario_id = session['user_id'] 
+          usuario_id = session['user_id']
 
-     producto_id = request.form.get('producto_id', type=int)
-     cantidad_vendida = request.form.get('cantidad', type=int)
+          producto_id = request.form.get('producto_id', type=int)
+          cantidad_vendida = request.form.get('cantidad', type=int)
     
      if not producto_id or not cantidad_vendida or cantidad_vendida <= 0:
          flash("❌ Selecciona un producto y cantidad válida", "error")
