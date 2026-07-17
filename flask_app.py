c142122fb98c6b7326127a6958d7b2cd15d1fa52
misaelmay49-wq <misaelmay49@gmail.com>
2026-07-17 14:45:33 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 
-flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify 
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify 
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 from logica import registrar_producto, obtener_productos, procesar_venta_logica, get_conn
