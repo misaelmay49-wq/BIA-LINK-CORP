efd09408e38b6f02b71055e3582e795ee01e6aaa
misaelmay49-wq <misaelmay49@gmail.com>
2026-07-25 21:21:48 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from datetime import timedelta
 import uuid
+from decimal import Decimal, InvalidOperation
 from logica import registrar_producto, procesar_venta_logica, get_conn
 
 app = Flask(__name__)
