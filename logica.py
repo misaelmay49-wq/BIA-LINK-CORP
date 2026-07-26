2c2832e1c643e85d145341cef67f68530e01b032
misaelmay49-wq <misaelmay49@gmail.com>
2026-07-26 07:05:27 -0600

    Update logica.py

--- logica.py
+++ logica.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor 
-from decimal import Decimal 
+from decimal import Decimal, InvalidOperation 
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
