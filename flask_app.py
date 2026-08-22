a766b6143120850d0de80a2e44f9e519ba4b84f8
misaelmay49-wq <misaelmay49@gmail.com>
2026-08-22 09:11:56 -0600

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -24,6 +24,7 @@ def get_locale():
     return request.accept_languages.best_match(['es', 'en'])
 
 babel.init_app(app, locale_selector=get_locale)
+print(">>> 3. BABEL INICIALIZADO")
 
 @app.route('/')
 def inicio():
